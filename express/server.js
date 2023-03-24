260439ab035e5213954ebce1e87e976c284730ee
Dubi394 <95634728+Dubi394@users.noreply.github.com>
2023-03-24 12:42:45 +0300

    Update server.js

--- express/server.js
+++ express/server.js
@@ -1,63 +1,20 @@
-const express = require('express');
-const serverless = require('serverless-http');
+"use strict";
+const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
-const bodyParser = require('body-parser');
-
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-// const app = require('./app');
-
-dotenv.config({ path: './config.env' });
-
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
-
-mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+const bodyParser = require("body-parser");
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+router.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello from Express.js!</h1>");
   res.end();
 });
-
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
+router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router); // path must route to lambda
+app.use("/.netlify/functions/server", router); // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
-
-/*
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
-  console.log(`App running at port ${port}...`);
-});
-
-process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
-  console.log('UNHANDLED REJECTION! 💥 Shuting Down...');
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-process.on('uncaughtException', (err) => {
-  console.log('UNCAUGHT EXCEPTION! 💥 Shuting Down...');
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-process.on('SIGTERM', () => {
-  console.log('☢💥 SIGTERM RECIVED, Shutting down gracefly ');
-  server.close(() => {
-    console.log('💥 Process terminated');
-  });
-});
-*/
