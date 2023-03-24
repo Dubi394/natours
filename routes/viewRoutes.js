376c6afc5bedd41b3b0cb3105679350c274f12fe
Dubi394 <95634728+Dubi394@users.noreply.github.com>
2023-03-24 13:09:06 +0300

    Update viewRoutes.js

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,21 +1,22 @@
 const express = require('express');
 
+const app = express();
+const bodyParser = require('body-parser');
+
 const viewsContoller = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
-const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get(
-  '/',
-  bookingController.createBookingCheckout,
-  authController.isLoggedIn,
-  viewsContoller.getOverview
-);
+router.use(viewsContoller.alerts);
+
+router.get('/', authController.isLoggedIn, viewsContoller.getOverview);
+
 router.get('/tour/:slug', authController.isLoggedIn, viewsContoller.getTour);
 router.get('/login', authController.isLoggedIn, viewsContoller.getLoginForm);
 router.get('/singup', authController.isLoggedIn, viewsContoller.getSingupForm);
 router.get('/me', authController.protect, viewsContoller.getAccount);
+
 router.get('/my-tours', authController.protect, viewsContoller.getMyTours);
 
 router.post(
@@ -23,5 +24,8 @@ router.post(
   authController.protect,
   viewsContoller.updateUserData
 );
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router); // path must route to lambda
 
 module.exports = router;
